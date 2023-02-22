6df57a7f7065fff3a5ab3b0938504d3b509bd60e
bachtiar <bachtiar@privyid.tech>
2023-02-22 14:37:19 +0700

    debug

--- go.mod
+++ go.mod
@@ -8,12 +8,15 @@ require (
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	cloud.google.com/go/iam v0.8.0 // indirect
 	cloud.google.com/go/pubsub v1.28.0 // indirect
+	github.com/bachtiarfr/pubsub-logger/v2 v2.0.1 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/mod v0.8.0 // indirect
 	golang.org/x/net v0.6.0 // indirect
 	golang.org/x/oauth2 v0.5.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
--- publish_report.go
+++ publish_report.go
@@ -1,4 +1,4 @@
-package pubslih_report
+package publush_report
 
 import (
 	"context"
@@ -9,6 +9,36 @@ import (
 	"google.golang.org/api/option"
 )
 
+// func main() {
+// 	ctx := context.Background()
+
+// 	loggerData := entity.Logger{
+// 		ID:         	uuid.New(),
+// 		ServiceName: 	"ServiceName",
+// 		Request:    	map[string]any{},
+// 		RequestVendor:  map[string]any{},
+// 		Response:   	map[string]any{},
+// 		ResponseVendor: map[string]any{},
+// 		URL:        	"dummy",
+// 		UrlVendor:      "dummy",
+// 		Method:    	 	"POST",
+// 		StatusCode: 	200,
+// 		Event:      	"nodeflux_liveness",
+// 		RequestID:  	"state.RequestID",
+// 		Additional: 	map[string]any{},
+// 		Tag:        	"",
+// 		Error:      	map[string]any{},
+// 		CreatedAt:  	time.Now().Local(),
+// 	}
+
+// 	paramJson, _ := builderx.StructToMap(loggerData, "json")
+// 	errPublish := PublishReport(ctx, paramJson)
+// 	if errPublish != nil {
+// 		fmt.Printf("error when publish :", errPublish)
+// 	}
+// 	fmt.Printf("success publish")
+// }
+
 func PublishReport(ctx context.Context, data map[string]interface{}) error {
 	d, err := json.Marshal(data)
 	if err != nil {
@@ -16,8 +46,15 @@ func PublishReport(ctx context.Context, data map[string]interface{}) error {
 	}
 
 	projectID := "bachtiar-development"
-	credentialFilePath := "bachtiar-development-73ca13e5c16e.json"
+	credentialFilePath := "config/bachtiar-development-73ca13e5c16e.json"
 	topicID := "dev-logger-topic"
+
+	fmt.Printf("credential file :", credentialFilePath)
+	fmt.Printf("projectID :", projectID)
+	fmt.Printf("topicID :", topicID)
+
+	fmt.Printf("ctx :", ctx)
+	fmt.Printf("data :", data)
 
 	client, err := pubsub.NewClient(ctx, projectID, option.WithCredentialsFile(credentialFilePath))
 	if err != nil {
